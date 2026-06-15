a8781ea24d3f4eaa37889c87584a62613dd965f5
RahulVasudeva <rahulv1216@gmail.com>
2026-06-15 13:49:26 +0530

    added javax.json for the parser to use

--- sudoku/src/main/java/Parser.java
+++ sudoku/src/main/java/Parser.java
@@ -1,4 +1,8 @@
-public class Parser {
+import javax.json.Json;
+import javax.json.Json.*;
+import javax.json.JsonObject;
+
+public class Parser    {
     private sudokuGrid sg = new sudokuGrid();
 
     public void inputHandler(String inInput) {
@@ -7,9 +11,10 @@ public class Parser {
             while(true) {
                 sg.printGrid();
                 String input = IO.readln("rules to play");
-                input.replaceAll("\\s","");
-                
-
+                JsonObject inputJson = Json .createObjectBuilder().build();
+                inputJson = jsonCreater(input);
+                System.out.printf("%s",inputJson.getString("instruction"));
+                break;
 
             }
             //return;
@@ -19,4 +24,20 @@ public class Parser {
             return;
         }
     }
+
+    private JsonObject jsonCreater(String str) {
+        //removes all the white space
+        str.replaceAll("\\s","");
+        //ToDo: add a check to see the length is 4 exactly
+
+        JsonObject obj = Json.createObjectBuilder()
+                .add("instruction",str.substring(0,1))
+                .add("x-axis",Integer.parseInt(str.substring(1,2)))
+                .add("y-axis",Integer.parseInt(str.substring(2,3)))
+                .add("value",Integer.parseInt(str.substring(3)))
+                .build();
+
+        return obj;
+    }
+
 }
